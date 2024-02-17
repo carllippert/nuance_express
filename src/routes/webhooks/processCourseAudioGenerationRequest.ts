76f0b2b33c46cb1ceb952616b578b625d499f648
Carl Lippert <carl.lippert@gmail.com>
2024-02-17 17:01:35 -0500

    finagling with webhook

--- src/routes/webhooks/processCourseAudioGenerationRequest.ts
+++ src/routes/webhooks/processCourseAudioGenerationRequest.ts
@@ -14,14 +14,14 @@ routes.post("/", async (req, res) => {
         //If the request is new and is a request to generate immediately, then generate the course
         if (event.type === 'INSERT' && event.table === 'speech_course_assets') {
             let row = event.record;
-            await generateAudioForCourseMessage(row);
+            // await generateAudioForCourseMessage(row);
         }
 
         res.status(200).send();
 
     } catch (error) {
-        console.log("Error managing auth change for supabase user to send event to loops", error);
-        Sentry.captureMessage("Error managing auth change for supabase user to send event to loops");
+        console.log("Error creating audio assets for course", error);
+        Sentry.captureMessage("Error creating audio assets for course");
         Sentry.captureException(error);
         // throw error;
         res.status(500).send({ message: error.message });
