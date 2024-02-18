469d08ec82a35bc4f77d5ee8f4e7f7907546a833
Carl Lippert <carl.lippert@gmail.com>
2024-02-18 09:59:15 -0500

    pushing up course audio again

--- src/routes/webhooks/processCourseAudioGenerationRequest.ts
+++ src/routes/webhooks/processCourseAudioGenerationRequest.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as Sentry from "@sentry/node";
-// import type { Payload } from "./supabase_webhook_types";
-// import { generateAudioForCourseMessage } from "../../speechCourses/generateAudioForCourseMessage";
+import type { Payload } from "./supabase_webhook_types";
+import { generateAudioForCourseMessage } from "../../speechCourses/generateAudioForCourseMessage";
 
 const routes = Router();
 
@@ -9,13 +9,13 @@ routes.post("/", async (req, res) => {
     try {
         console.log("process-course-audio-generation body: ", req.body);
 
-        // let event: Payload = req.body;
+        let event: Payload = req.body;
 
         //If the request is new and is a request to generate immediately, then generate the course
-        // if (event.type === 'INSERT' && event.table === 'speech_course_assets') {
-        //     let row = event.record;
-        //     // await generateAudioForCourseMessage(row);
-        // }
+        if (event.type === 'INSERT' && event.table === 'speech_course_assets') {
+            let row = event.record;
+            await generateAudioForCourseMessage(row);
+        }
 
         res.status(200).send();
 
