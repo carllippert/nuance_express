9bc0b7e0b36bd0504cde2bce59aaa891138b250e
Carl Lippert <carl.lippert@gmail.com>
2024-03-28 16:58:42 -0500

    more cleanign up for word context for lemmas

--- src/routes/webhooks/processSystemWords.ts
+++ src/routes/webhooks/processSystemWords.ts
@@ -5,6 +5,18 @@ import { createClient } from "@supabase/supabase-js";
 import nlp from 'es-compromise'
 import { OxfordData } from "../../types/oxford";
 
+type WordContext = {
+    language: string;
+    word_id: string;
+    word_original_format: string;
+    nlp_blob?: any;
+    noun_points: number;
+    root?: string;
+    oxford_lemma?: string;
+    oxford_lemma_blob?: OxfordData;
+    capitalized: boolean;
+}
+
 const routes = Router();
 
 //Get words
@@ -30,7 +42,7 @@ routes.get("/", async (req, res) => {
             .select("*")
             .eq("processing_status", "NEW")
             .order('created_at', { ascending: true })
-            .limit(10) //TODO: change to 10 when ready
+            .limit(100) //TODO: change to 10 when ready
 
         if (error) throw new Error(error.message);
 
@@ -38,43 +50,48 @@ routes.get("/", async (req, res) => {
         // let proper_nouns = [];
         // let non_proper_noun_nlp_blobs = [];
 
-        let scored_words = [];
+        // let scored_words = [];
 
         let lemma_calls = [];
 
         data.forEach(async (word) => {
-            let noun_points = 0;
+
+            const wordContext: WordContext = {
+                language: word.language,
+                word_id: word.word_id,
+                word_original_format: word.word_original_format,
+                // nlp_blob: null,
+                noun_points: 0,
+                // root: '',
+                // oxford_lemma: '',
+                capitalized: false
+            };
+
+            // let noun_points = 0;
 
             let capitalized = word.word_original_format[0] === word.word_original_format[0].toUpperCase();
 
             if (capitalized) {
-                noun_points += 1;
+                wordContext.noun_points += 1;
+                wordContext.capitalized = true;
             }
 
             // console.log("Word: ", word);
             let doc = nlp(word.word_id);
-            doc.compute('root');
-            doc.compute('machine')
-            // doc.compute('penn');
+            doc.compute('root'); // get lemma
             let json = doc.json();
-            console.log("doc: ", JSON.stringify(json, null, 2));
+            // console.log("doc: ", JSON.stringify(json, null, 2));
 
             //check if "ProperNoun" exists in terms
             let properNoun = json[0].terms.find(term => term.tags.includes("ProperNoun"));
             let noun = json[0].terms.find(term => term.tags.includes("Noun"));
 
             if (properNoun) {
-                noun_points += 1;
-                // console.log("ProperNoun: ", JSON.stringify(properNoun, null, 2));
-                // proper_nouns.push(properNoun);
-                // proper_nounds.push(properNoun.text);
-                // break;
+                wordContext.noun_points += 1;
             }
 
             if (noun) {
-                noun_points += 1;
-                // console.log("Noun: ", JSON.stringify(noun, null, 2));
-                // nouns.push(noun);
+                wordContext.noun_points += 1;
             }
 
             //add root word to the word
@@ -82,20 +99,20 @@ routes.get("/", async (req, res) => {
             // console.log("Root: ", JSON.stringify(root, null, 2));
 
             // let scored = { ...json[0], noun_points, root: root.text };
-            let scored: any = { noun_points, text: word.word_id, original_format: word.word_original_format };
+            // let scored: any = { noun_points, text: word.word_id, original_format: word.word_original_format };
 
             if (root != undefined) {
-                scored.root = root;
+                wordContext.root = root;
             }
 
             // scored_words.push(scored);
-            console.log("Scored Words: ", JSON.stringify(scored, null, 2));
+            // console.log("Scored Words: ", JSON.stringify(scored, null, 2));
             // {
             //     console.log("NOT ProperNoun: ", JSON.stringify(json[0], null, 2));
             //     non_proper_noun_nlp_blobs.push(json[0]);
             // }
 
-            lemma_calls.push(getLemma("es", word.word_id));
+            lemma_calls.push(getLemma(wordContext));
         });
 
         let lemmas = await Promise.all(lemma_calls);
@@ -194,15 +211,16 @@ const getDefinition = async (language: string, word: string) => {
     }
 };
 
-const getLemma = async (language: string, word: string) => {
+const getLemma = async (word: WordContext) => {
 
-    // const fields = "definitions";
-    // const strictMatch = "false";
+    //use root if we have it
+    let word_text = word.root || word.word_id;
+
     const path =
         "/lemmas/" +
-        language +
+        word.language +
         "/" +
-        word;
+        word_text;
 
     console.log("Path -> " + path);
 
@@ -226,15 +244,21 @@ const getLemma = async (language: string, word: string) => {
         console.log(body, null, 3);
         const parsed: OxfordData = JSON.parse(body);
 
-        const lemmaWord = parsed.results[0].lexicalEntries[0].inflectionOf?.[0].text;
+        const lemmaWord = parsed.results?.[0]?.lexicalEntries?.[0]?.inflectionOf?.[0]?.text;
 
         if (lemmaWord) {
             // const data = await getDefinition(language, lemmaWord);
-            return lemmaWord;
+
+            word.oxford_lemma = lemmaWord;
+            word.oxford_lemma_blob = parsed;
+            // return word;
         } else {
-            console.log("Didnt grab lemma word...");
-            throw new Error("No Inflection Found via Lemma");
+            console.log("Didnt grab lemma word for ...");
+            console.log(JSON.stringify(body, null, 3));
+            // return word;
         }
+        console.log("Word: ", JSON.stringify(word, null, 2));
+        return word;
     } catch (err) {
         throw err;
     }
